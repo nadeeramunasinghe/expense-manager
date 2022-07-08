df1c4664f413031ce408d7b85c2cf2fe2430d98f
Nadeera <ntnadeeram@gmail.com>
2022-07-08 22:27:36 +0530

    all

---
+++ app/src/main/java/com/nadeera/expensemanager/Activities/ForgotPasswordActivity.java
@@ -0,0 +1,16 @@
+package com.nadeera.expensemanager.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nadeera.expensemanager.R;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
---
+++ app/src/main/java/com/nadeera/expensemanager/Activities/MonthSpendingActivity.java
@@ -0,0 +1,16 @@
+package com.nadeera.expensemanager.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nadeera.expensemanager.R;
+
+public class MonthSpendingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_month_spending);
+    }
+}
